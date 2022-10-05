02c6e318d3c9eddd6281f1e167be985a0284c7b5
Lee Angelina <angelinalee_3110@mail.ru>
2022-10-05 22:57:22 +0600

    change login screen

--- src/components/CustomInput/CustomInput.js
+++ src/components/CustomInput/CustomInput.js
@@ -17,15 +17,17 @@ const CustomInput = ({ value, setValue, placeholder, secureTextEntry}) => {
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: 'white',
+    backgroundColor: '#D3D1E2',
     width: '80%',
-    height: 50,
+    height: 45,
+    color: '#827878',
+    borderRadius: 30,
 
-    borderColor: '#e8e8e8',
-    borderWidth: 1,
-    borderRadius: 5,
+    // borderColor: '#e8e8e8',
+    // borderWidth: 1,
+    // borderRadius: 5,
 
-    paddingHorizontal: 10,
+    paddingHorizontal: 20,
     marginVertical: 10,
   },
   input: {
