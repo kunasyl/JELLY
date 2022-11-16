e47970e434800c0a24072380bcb275c1d3a994b1
Nurasyl <k.nurasyl7788@gmail.com>
2022-11-16 13:20:26 +0600

    Сделал логику чата

--- src/screens/ChatScreen/ChatScreen.js
+++ src/screens/ChatScreen/ChatScreen.js
@@ -4,9 +4,11 @@ import { useNavigation } from '@react-navigation/native'
 import { COLORS } from '../../styles/colors'
 
 import { NativeBaseProvider, Text, Box, Heading, Spacer, Avatar, FlatList, HStack, VStack, Input,
-Container, Header, Title, Button, Left, Right, Body, Icon, Center} from "native-base";
+Container, Header, Title, Button, Left, Right, Body, Icon, Center, SafeAreaView, TextInput } from "native-base";
 
 const ChatScreen = () => {
+  const [text, setText] = useState('');
+  const [submitState, setSubmitState] = useState(false);
   const [messages, setMessage] = useState([]);
 
   const getMessage = async (messages) => {
@@ -26,14 +28,57 @@ const ChatScreen = () => {
     setMessage(messages);
   };
 
+  const postMessage = async (message, author) => {
+      await fetch('http://146.185.154.90:8000/messages', {
+          method: 'POST',
+          body: new URLSearchParams({
+              message: message,
+              author: author
+          })
+      }).then(res => {
+          return res.json();
+      });
+  };
+
   const SendMessage = () => {
-    
+    setSubmitState(!submitState);
+    postMessage(text, 'JELLY');
   };
 
   useEffect(() => {
     let messagesCopy = [...messages];
     getMessage(messagesCopy);
-  }, [messages]);
+  }, [submitState]);
+
+  useEffect(() => {
+    let messageCopy = [...messages];
+    const updateMessage = async (messageObj) => {
+      const response = await fetch('http://146.185.154.90:8000/messages');
+      const data = await response.json();
+      let lastDate = data.slice(-1)[0].datetime;
+
+
+      const lastResponse = await fetch('http://146.185.154.90:8000/messages?datetime=' + `${lastDate}`);
+      const lastData = await lastResponse.json();
+
+      if(lastData.length !== 0) {
+        messageObj = lastData.map(content => {
+          const newMessage = {
+            id: content._id + uuidv4(),
+            message: content.message,
+            author: content.author,
+            date: content.datetime
+          };
+          return newMessage;
+        }).reverse();
+      };
+    };
+
+    setInterval(() => {
+      updateMessage(messageCopy);
+    }, 2000)
+  }, [])
+
 
   return (
     <NativeBaseProvider>
@@ -47,28 +92,24 @@ const ChatScreen = () => {
                   <Avatar size="48px" source={{uri: 'https://cdn2.iconfinder.com/data/icons/audio-16/96/user_avatar_profile_login_button_account_member-512.png'}} />
                     <VStack>
                       <Text _dark={{color: "warmGray.50"}} color={COLORS.dark} bold>
-                        {item.author}
+                        {item?.author}
                       </Text>
                       <Text color={COLORS.dark} _dark={{color: "warmGray.200"}}>
-                        {item.message}
+                        {item?.message}
                       </Text>
                     </VStack>
                     <Spacer ccolor={COLORS.dark} />
                     <Text fontSize="xs" _dark={{color: "warmGray.50"}} color={COLORS.purple} alignSelf="flex-start">
-                      {new Date(Date(item.datetime)).toLocaleDateString()}
-                    </Text>
-                    <Text fontSize="xs" _dark={{color: "warmGray.50"}} color={COLORS.purple} alignSelf="flex-start">
-                      {new Date(Date(item.datetime)).toLocaleTimeString()}
+                      {new Date(Date(item?.datetime)).toLocaleDateString()}
                     </Text>
                   </HStack>
-                </Box>} keyExtractor={item => item._id} />  */}
-                
+                </Box>} keyExtractor={item => item?._id} />
               </ScrollView>
             </Box> 
             <Box>
-              {/* <Input style={{backgroundColor: COLORS.grey}} py="0" px="5" my="5" InputRightElement={
+              <Input style={{backgroundColor: COLORS.grey}} py="0" px="5" my="5" InputRightElement={
                 <Button  size="sm" rounded="50" w="1/5" style={{backgroundColor: COLORS.purple}} onPress={SendMessage}> Send </Button>}
-              placeholder="message" variant="rounded" value={valueMessage} onPress={changeMessage}/> */}
+              placeholder="message" variant="rounded" onChangeText={setText} value={text}/>
             </Box>
           </VStack>
       </Center>
